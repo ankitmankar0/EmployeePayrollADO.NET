2e952e715c5d6e3206edd88aafa1c22f3a216055
ankitmankar0 <ankitmankar012@gmail.com>
2022-04-07 23:31:45 +0530

    ADO.NET

---
+++ AdoNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdoNet
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Services Using ADO.NET Problem");
+
+            EmployeeRepo repository = new EmployeeRepo();  //Creating a object of EmployeeRepository class.
+
+            // UC1 Ensuring the database connection using the sql connection string
+            repository.DataBaseConnection();
+
+
+        }
+
+
+    }
+}
